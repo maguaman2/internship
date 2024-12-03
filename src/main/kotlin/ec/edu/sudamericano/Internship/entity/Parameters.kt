bd237efcc931ef1755517f2c69f8da848c368920
edmendezvilla <146900895+edmendezvilla@users.noreply.github.com>
2024-12-02 21:58:25 -0500

    avance

---
+++ src/main/kotlin/ec/edu/sudamericano/Internship/entity/Parameters.kt
@@ -0,0 +1,13 @@
+package ec.edu.sudamericano.Internship.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "parameters")
+
+
+
+class Parameters {
+
+}
