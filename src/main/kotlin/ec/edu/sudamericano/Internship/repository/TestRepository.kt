e10479374f0e8d22c307336d1bc9cc9039c50579
Alexnarea <lanarea@sudamericano.edu.ec>
2024-12-02 21:56:47 -0500

    avance

--- src/main/kotlin/ec/edu/sudamericano/Internship/repository/TestRepository.kt
+++ src/main/kotlin/ec/edu/sudamericano/Internship/repository/TestRepository.kt
@@ -1,4 +1,10 @@
 package ec.edu.sudamericano.Internship.repository
 
-interface TestRepository {
+import ec.edu.sudamericano.Internship.entity.Test
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface TestRepository : JpaRepository<Test, Long>{
+
 }
