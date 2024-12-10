95d01ba8bdd43ec9a69500cfd880028f54d3a660
javera91 <146044960+javera91@users.noreply.github.com>
2024-12-09 21:19:34 -0500

    new changes added Response

--- src/main/kotlin/ec/edu/sudamericano/Internship/controller/EngagementCoordinatorController.kt
+++ src/main/kotlin/ec/edu/sudamericano/Internship/controller/EngagementCoordinatorController.kt
@@ -1,12 +1,13 @@
 package ec.edu.sudamericano.Internship.controller
 
 import ec.edu.sudamericano.Internship.dto.EngagementCoordinatorDto
-import ec.edu.sudamericano.Internship.entity.EngagementCoordinator
+import ec.edu.sudamericano.Internship.response.SuccessResponse
 import ec.edu.sudamericano.Internship.service.EngagementCoordinatorService
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-
 
 @RestController
 @RequestMapping("/api/engagementCoordinators")
@@ -16,25 +17,33 @@ class EngagementCoordinatorController {
     lateinit var engagementCoordinatorService: EngagementCoordinatorService
 
     @GetMapping
-    fun getEngagementCoordinators() = engagementCoordinatorService.getEngagementCoordinators()
+    fun getEngagementCoordinators(): ResponseEntity<*> {
+        val response = engagementCoordinatorService.getEngagementCoordinators()
+        return ResponseEntity(SuccessResponse(status = "success", data = response), HttpStatus.OK)
+    }
 
     @GetMapping("/{id}")
-    fun getEngagementCoordinatorById(@PathVariable id: Long): EngagementCoordinator {
-        return engagementCoordinatorService.getEngagementCoordinatorById(id)
+    fun getEngagementCoordinatorById(@PathVariable id: Long): ResponseEntity<*> {
+            val response = engagementCoordinatorService.getEngagementCoordinatorById(id)
+            return ResponseEntity(SuccessResponse(status = "success", data = response), HttpStatus.OK)
     }
 
     @PostMapping
-    fun save(@RequestBody @Valid engagementCoordinatorDto: EngagementCoordinatorDto): EngagementCoordinator{
-        return engagementCoordinatorService.save(engagementCoordinatorDto)
+    fun save(@RequestBody @Valid engagementCoordinatorDto: EngagementCoordinatorDto): ResponseEntity<*> {
+            val response = engagementCoordinatorService.save(engagementCoordinatorDto)
+            return ResponseEntity(SuccessResponse(status = "success", data = response), HttpStatus.CREATED)
     }
 
     @PutMapping("/{id}")
-    fun updateEngagementCoordinator(@PathVariable id: Long, @RequestBody @Valid engagementCoordinatorDto: EngagementCoordinatorDto): EngagementCoordinator{
-        return engagementCoordinatorService.updateEngagementCoordinator(id, engagementCoordinatorDto)
+    fun updateEngagementCoordinator(@PathVariable id: Long, @RequestBody @Valid engagementCoordinatorDto: EngagementCoordinatorDto): ResponseEntity<*> {
+            val response = engagementCoordinatorService.updateEngagementCoordinator(id, engagementCoordinatorDto)
+            return ResponseEntity(SuccessResponse(status = "success", data = response), HttpStatus.OK)
+
     }
 
     @DeleteMapping("/{id}")
-    fun deleteEngagementCoordinator(@PathVariable id: Long) {
+    fun deleteEngagementCoordinator(@PathVariable id: Long): ResponseEntity<*> {
         engagementCoordinatorService.deleteEngagementCoordinator(id)
+        return ResponseEntity(SuccessResponse(status = "success", data = "Coordinator deleted"), HttpStatus.NO_CONTENT)
     }
 }
