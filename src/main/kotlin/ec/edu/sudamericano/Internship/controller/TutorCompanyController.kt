d44527f5d50e0cf00657aec011dd05901c5b23bb
Victoria-94 <146044863+Victoria-94@users.noreply.github.com>
2024-12-11 19:38:33 -0500

    actualizacion tabla tutor company

--- src/main/kotlin/ec/edu/sudamericano/Internship/controller/TutorCompanyController.kt
+++ src/main/kotlin/ec/edu/sudamericano/Internship/controller/TutorCompanyController.kt
@@ -1,15 +1,16 @@
 package ec.edu.sudamericano.Internship.controller
 
+
+
 import ec.edu.sudamericano.Internship.dto.TutorCompanyDto
-import ec.edu.sudamericano.Internship.entity.TutorCompanyEntity
+
+import ec.edu.sudamericano.Internship.response.SuccessResponse
 import ec.edu.sudamericano.Internship.service.TutorCompanyService
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/tutorCompanies")
@@ -17,13 +18,37 @@ class TutorCompanyController {
     @Autowired
     lateinit var tutorCompanyService: TutorCompanyService
 
-    @GetMapping
-    fun list(): List<TutorCompanyEntity>{
-        return tutorCompanyService.list()
-    }
+    @GetMapping("/{id}")
+        fun findAll(): ResponseEntity<*> {
+            val response = tutorCompanyService.findAll()
+            return ResponseEntity(SuccessResponse(data=response), HttpStatus.OK)
+        }
 
-    @PostMapping
-    fun save (@RequestBody @Valid tutorCompanyDto: TutorCompanyDto): TutorCompanyDto {
-        return tutorCompanyService.save(tutorCompanyDto)
-    }
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): ResponseEntity<*> {
+            val response = tutorCompanyService.findById(id)
+            return ResponseEntity(SuccessResponse(data = response), HttpStatus.OK)
+        }
+
+    @PostMapping("/{id}")
+    fun save(@RequestBody @Valid tutorCompanyDto: TutorCompanyDto): ResponseEntity<*> {
+            val response= tutorCompanyService.save(tutorCompanyDto)
+            return ResponseEntity(SuccessResponse(data = response), HttpStatus.CREATED)
+        }
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable id: Long, @RequestBody @Valid tutorCompanyDto: TutorCompanyDto): ResponseEntity<*> {
+            val response = tutorCompanyService.update(id, tutorCompanyDto)
+
+        return ResponseEntity(SuccessResponse(data = response), HttpStatus.OK)
+
+        }
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Long): ResponseEntity<Any> {
+            val response = tutorCompanyService.delete(id)
+            return ResponseEntity(SuccessResponse(data = response), HttpStatus.OK)
+        }
 }
+
+
+
--- src/main/kotlin/ec/edu/sudamericano/Internship/service/TutorCompanyService.kt
+++ src/main/kotlin/ec/edu/sudamericano/Internship/service/TutorCompanyService.kt
@@ -1,24 +1,61 @@
 package ec.edu.sudamericano.Internship.service
 
+
 import ec.edu.sudamericano.Internship.dto.TutorCompanyDto
 import ec.edu.sudamericano.Internship.entity.TutorCompanyEntity
 import ec.edu.sudamericano.Internship.mapper.TutorCompanyMapper
+
 import ec.edu.sudamericano.Internship.repository.TutorCompanyRepository
+
+import jakarta.persistence.EntityNotFoundException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class TutorCompanyService {
-    @Autowired
-    lateinit var  tutorCompanyRepository: TutorCompanyRepository
 
-    fun list(): List<TutorCompanyEntity> {
-        return tutorCompanyRepository.findAll()
-    }
+        @Autowired
+        lateinit var tutorCompanyRepository: TutorCompanyRepository
 
-    fun save (tutorCompanyDto: TutorCompanyDto): TutorCompanyDto{
-        val tutorCompanyEntity= TutorCompanyMapper.toEntity(tutorCompanyDto)
-        val savedTutorCompany= tutorCompanyRepository.save(tutorCompanyEntity)
-        return TutorCompanyMapper.toDto( savedTutorCompany)
+
+        fun findAll(): List<TutorCompanyDto> {
+            val TutorCompanys = tutorCompanyRepository.findAll()
+            return TutorCompanys.map {TutorCompanyMapper.toDto(it)}
+        }
+
+
+        fun findById(id: Long): TutorCompanyEntity {
+            val tutorCompany = tutorCompanyRepository.findById(id)
+                .orElseThrow{ EntityNotFoundException("TutorCompany with id $id not found") }
+            return tutorCompany
+        }
+
+
+        fun save(tutorCompanyDto: TutorCompanyDto): TutorCompanyEntity {
+            val tutorCompany = TutorCompanyMapper.toEntity(tutorCompanyDto)
+            val saveTutorCompany = tutorCompanyRepository.save(tutorCompany)
+            return saveTutorCompany
+        }
+
+
+        fun update(id: Long, tutorCompanyDto: TutorCompanyDto): TutorCompanyEntity {
+            val tutorCompany = tutorCompanyRepository.findById(id)
+                .orElseThrow{ EntityNotFoundException("TutorCompany with id $id not found") }
+            tutorCompany.apply {
+                fullName = tutorCompanyDto.fullName
+                roll = tutorCompanyDto.roll
+                activity = tutorCompanyDto.activity
+                observation = tutorCompanyDto.observation
+                companyId= tutorCompanyDto.companyId
+            }
+            val updateTutorCompany = tutorCompanyRepository.save(tutorCompany)
+            return updateTutorCompany
+        }
+
+
+        fun delete(id: Long) {
+            val tutorCompany = tutorCompanyRepository.findById(id)
+                .orElseThrow{ EntityNotFoundException("TutorCompany with id $id not found") }
+            tutorCompanyRepository.delete(tutorCompany)
+        }
     }
-}
