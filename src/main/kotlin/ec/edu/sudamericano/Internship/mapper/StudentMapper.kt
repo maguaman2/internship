ab8f8fd2ad7bfcc3bc214ae235210b304dbc0167
jdllanghi <146900717+jdllanghi@users.noreply.github.com>
2024-12-16 20:03:34 -0500

    controller

--- src/main/kotlin/ec/edu/sudamericano/Internship/mapper/StudentMapper.kt
+++ src/main/kotlin/ec/edu/sudamericano/Internship/mapper/StudentMapper.kt
@@ -1,4 +1,38 @@
 package ec.edu.sudamericano.Internship.mapper
+package ec.edu.sudamericano.Internship.mapper
 
-class StudentMapper {
+import ec.edu.sudamericano.Internship.dto.StudentDto
+import ec.edu.sudamericano.Internship.entity.Student
+
+object StudentMapper {
+    fun toEntity(studentDto: StudentDto): Student {
+        val student = Student()
+        student.id = studentDto.id
+        student.fullName = studentDto.fullName
+        student.nui = studentDto.nui
+        student.telephone = studentDto.telephone
+        student.email = studentDto.email
+        student.cycle = studentDto.cycle
+        student.section = studentDto.section
+        student.address = studentDto.address
+        student.photo_url = studentDto.photoUrl
+        student.career_id = studentDto.careerId
+        return student
+    }
+
+    fun toStudentDto(student: Student) {
+        val studentDto = StudentDto()
+       studentDto.id = student.id
+        studentDto.fullName = student.fullName
+        studentDto.nui = student.nui
+        studentDto.telephone = student.telephone
+        studentDto.email = student.email
+        studentDto.cycle = student.cycle
+        studentDto.section = student.section
+        studentDto.address = student.address
+        studentDto.photoUrl = student.photo_url
+        studentDto.careerId = student.career_id
+
+    }
 }
+
--- src/main/kotlin/ec/edu/sudamericano/Internship/service/StudentService.kt
+++ src/main/kotlin/ec/edu/sudamericano/Internship/service/StudentService.kt
@@ -1,4 +1,42 @@
 package ec.edu.sudamericano.Internship.service
 
+
+import ec.edu.sudamericano.Internship.dto.StudentDto
+import ec.edu.sudamericano.Internship.entity.Student
+import ec.edu.sudamericano.Internship.mapper.StudentMapper
+import ec.edu.sudamericano.Internship.repository.StudentRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
 class StudentService {
+
+    @Autowired
+    lateinit var studentRepository: StudentRepository
+    fun getStudents(): List<Student> {
+        return studentRepository.findAll()
+    }
+
+
+    fun getStudentById(id: Long): Student {
+       return studentRepository.findById(id).orElseThrow {
+           throw RuntimeException("student not found with id $id")
+       }
+
+    }
+
+    fun updateStudent(id: Long, studentDto: StudentDto): Student {
+        val existingStudent = getStudentById(id)
+        val updatedStudent = StudentMapper.toEntity(studentDto).apply {
+            this.id = existingStudent.id
+        }
+        return studentRepository.save(updatedStudent)
+    }
+
+    fun deleteStudent(id: Long) {
+        if (!studentRepository.existsById(id)) {
+            throw RuntimeException("Student not found with id: $id")
+        }
+        studentRepository.deleteById(id)
+    }
 }
