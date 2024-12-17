01af5d926a4914110f4435693ec08fac07d4d57d
Johanna Banegas <jmbanegas@sudamericano.edu.ec>
2024-12-16 21:21:30 -0500

    comit

--- src/main/kotlin/ec/edu/sudamericano/Internship/entity/Category.kt
+++ src/main/kotlin/ec/edu/sudamericano/Internship/entity/Category.kt
@@ -2,15 +2,15 @@ package ec.edu.sudamericano.Internship.entity
 
 import jakarta.persistence.*
 
-
 @Entity
 @Table(name = "category")
-class category {
+class Category {
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Id
         @Column(updatable = false)
         var id: Long? = null
+
         var description: String? = null
         var signature: String? = null
-        var career: Long? = null
-    }
+        var careerId: Int? = null
+}
--- src/main/kotlin/ec/edu/sudamericano/Internship/mapper/CategoryMapper.kt
+++ src/main/kotlin/ec/edu/sudamericano/Internship/mapper/CategoryMapper.kt
@@ -1,4 +1,27 @@
 package ec.edu.sudamericano.Internship.mapper
 
-class CategoryMapper {
+import ec.edu.sudamericano.Internship.dto.CategoryDto
+import ec.edu.sudamericano.Internship.entity.Category
+import org.springframework.stereotype.Component
+
+@Component
+object CategoryMapper {
+
+    fun toEntity(categoryDto: CategoryDto): Category {
+        val category = Category()
+        category.id = categoryDto.id
+        category.description = categoryDto.description
+        category.careerId = categoryDto.careerId
+        category.signature = categoryDto.signature
+        return category
+    }
+
+    fun toCategoryDto(category: Category): CategoryDto {
+        val categoryDto = CategoryDto()
+        categoryDto.id = category.id
+        categoryDto.description = category.description
+        categoryDto.careerId = category.careerId
+        categoryDto.signature = category.signature
+        return categoryDto
+    }
 }
