4bd0465c3aaef7bbf6d6673592598cc880ecfc05
Mesias02 <146044728+Mesias02@users.noreply.github.com>
2024-12-18 19:02:50 -0500

    commit

--- src/main/kotlin/ec/edu/sudamericano/Internship/entity/InstituteEntity.kt
+++ src/main/kotlin/ec/edu/sudamericano/Internship/entity/InstituteEntity.kt
@@ -1,17 +1,15 @@
 package ec.edu.sudamericano.Internship.entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 
 @Entity
 @Table(name = "institute")
 class Institute {
-    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column(updatable = false, unique = true)
+
     val id: Long? = null
     var fullName: String? = null
     var address: String? = null
