ad03426b9dcc7f2d8a46ea910cf05321e64138fb
Victoria-94 <146044863+Victoria-94@users.noreply.github.com>
2024-12-18 19:01:07 -0500

    jpaTutorCompany

--- src/main/kotlin/ec/edu/sudamericano/Internship/repository/StudentRepository.kt
+++ src/main/kotlin/ec/edu/sudamericano/Internship/repository/StudentRepository.kt
@@ -1,4 +1,16 @@
 package ec.edu.sudamericano.Internship.repository
 
-class StudentRepository {
+import ec.edu.sudamericano.Internship.entity.TutorCompanyEntity
+import ec.edu.sudamericano.Internship.entity.student
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.NativeQuery
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+
+@Repository
+interface StudentRepository: JpaRepository<student, Long> {
+    fun findById(id:Long?): student?
+    @Query(nativeQuery = true)
+    fun findAllStudentCompany(@Param("companyId") companyId:Long?,):List
 }
--- src/main/kotlin/ec/edu/sudamericano/Internship/service/StudentService.kt
+++ src/main/kotlin/ec/edu/sudamericano/Internship/service/StudentService.kt
@@ -1,4 +1,17 @@
 package ec.edu.sudamericano.Internship.service
 
+import ec.edu.sudamericano.Internship.entity.student
+import ec.edu.sudamericano.Internship.repository.StudentRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
 class StudentService {
+    @Autowired
+    lateinit var studentRepository: StudentRepository
+
+    fun getstudentcompany (companyId:Long): List <student>{
+        return  studentRepository.findAllStudentCompany (companyId)
+
+    }
 }
